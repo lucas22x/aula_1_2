3089b61c030070352949fdb12def8b1c86fac9be
lucas22x <lucas.barbosa.f.oficial@gmail.com>
2025-07-22 16:11:55 -0300

    teste_2

--- src/aula_1_2/crew.py
+++ src/aula_1_2/crew.py
@@ -2,7 +2,7 @@ from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
 from crewai.agents.agent_builder.base_agent import BaseAgent
 from typing import List
-
+ 
 
 @CrewBase
 class Aula12():
@@ -12,7 +12,7 @@ class Aula12():
     tasks: List[Task]
 
 
-# agentes //////////////////////
+# agentes /////////////////////
 
     @agent
     def researcher(self) -> Agent:
